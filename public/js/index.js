19ed3effffa26120f8f088d3030221f391e043ff
Leonardo Xavier <101280914+leeoxavi@users.noreply.github.com>
2023-02-06 09:22:42 -0300

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+const compra = document.getElementById('cart--finalizar')
+
+function comprar(){
+    const log = alert( 'seu pedido foi para a produção e já será enviado para a entrega, obrigado pela preferência.')
+}
+
+compra.addEventListener( 'click', comprar);
